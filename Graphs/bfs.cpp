59d62742b55c32c968fe68263e34c5ae8b6a96ae
ToniBirat7 <gotofreelancing1@gmail.com>
2026-06-22 15:35:00 +0545

    graph

--- Graphs/bfs.cpp
+++ Graphs/bfs.cpp
@@ -1,1 +1,45 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Graph {
+private: 
+    int numVertices;
+    vector<vector<int>> adjList;
+
+public:
+    Graph(int vertices) {
+        numVertices = vertices;
+        adjList.resize(vertices);
+    }
+
+    // Add an edge for an undirected graph
+    void addEdge(int u, int v) {
+        adjList[u].push_back(v);
+    }
+
+    // Print the representation
+    void printGraph() {
+        for (int i = 0; i < numVertices; i++) {
+            cout << "Vertex " << i << " is connected to: ";
+            for (int neighbour : adjList[i]) {
+                cout << neighbour << " ";
+            }
+            cout << "\n";
+        }
+    }
+};
+
+int main() {
+
+    Graph g(4);
+    g.addEdge(0,1);
+    g.addEdge(0,2);
+    g.addEdge(1,2);
+    g.addEdge(2,3);
+
+    g.printGraph();
+
+    return 0;
+}
