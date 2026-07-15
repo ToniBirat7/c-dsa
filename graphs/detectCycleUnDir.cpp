eb39771f3f784a2111a338e45c7478bbb7156ddd
ToniBirat7 <gotofreelancing1@gmail.com>
2026-07-15 12:35:29 +0545

    graph: self detect cycle undirected

--- graphs/detectCycleUnDir.cpp
+++ graphs/detectCycleUnDir.cpp
@@ -60,14 +60,10 @@ int main() {
 
     g.addEdge(0, 1);
     g.addEdge(0, 2);
-    g.addEdge(1, 3);
-    g.addEdge(1, 4);
-    g.addEdge(2, 4);
-    g.addEdge(2, 5);
-    g.addEdge(3, 6);
-    g.addEdge(4, 6);
-    g.addEdge(4, 7);
-    g.addEdge(6, 7);
+    g.addEdge(0, 3);
+    g.addEdge(1, 2);
+    g.addEdge(3, 4);
+
 
     g.printGraph();
 
